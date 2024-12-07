6494550a97348dcd7378aa26b209bfa94f3aa4c8
ki-ikki <25ikik25@gmail.com>
2024-12-08 01:53:55 +0900

    fix: envファイル修正

--- config/settings/base.py
+++ config/settings/base.py
@@ -15,9 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
-# SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-20nf2_rib#ezb*mm^w69ck+r=*ptx=6e2af9zs_@oa(+06xso_'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
